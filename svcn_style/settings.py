3b9c04cc29e1b05afb0d316e3c293439122e1766
QuantumX00 <nf970004@gmail.com>
2026-06-20 20:27:46 -0500

    Corregir rutas de estaticos

--- svcn_style/settings.py
+++ svcn_style/settings.py
@@ -115,15 +115,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'tienda', 'static'),
-]
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # AGREGA ESTO PARA LAS FOTOS DE ROPA:
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
 LOGIN_REDIRECT_URL = 'index'
 LOGOUT_REDIRECT_URL = 'index'
 LOGIN_URL = 'login'
