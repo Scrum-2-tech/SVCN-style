562ac124bdef60c65e882205e59a1eed6c4e3a98
QuantumX00 <nf970004@gmail.com>
2026-06-20 20:29:51 -0500

    Corregir statics

--- svcn_style/settings.py
+++ svcn_style/settings.py
@@ -115,6 +115,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'tienda', 'static'),
+]
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # AGREGA ESTO PARA LAS FOTOS DE ROPA:
